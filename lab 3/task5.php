bc8de340398084afc032a426b1c71b68c682c021
Arifuzzman niaz <niazarifuzzman99@gmail.com>
2021-06-14 23:26:49 +0600

    update

---
+++ lab 3/task5.php
@@ -0,0 +1,21 @@
+<?php
+    $backcolor="White";
+    $imagecolor="Red";
+    $imageWidth="720px";
+    $imageHeight="1280px";
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset= "utf-8">
+</head>
+<body>
+    <table>
+        <tr>
+            <td><img src= "C:\Users\Babu\Desktop\test\niaz.jpg" height="<?php $imageHeight ?>" Width="<?php $imageWidth ?>"></td>
+            <td><span><h2 style="background-color:<?php echo $backcolor;?>; color: <?php echo $fontcolor;?>;"
+            <h2>American International University-Bangladesh</h2></span></td>
+        </tr>
+    </table>
+</body>
+</html>
